1eb883598aab048d8f2331832c4f9e7ed338499d
j-hassan <imuhammadhassanktk@gmail.com>
2024-01-24 10:14:06 +0500

    new player

---
+++ js/music-list.js
@@ -0,0 +1,23 @@
+
+let allMusic = [
+  {
+    name: "Hamd",
+    artist: "Ali Zafar",
+    src: "hamd"
+  },
+  {
+    name: "Banda-e-Asii",
+    artist: "Hassan Ullah Hussani",
+    src: "ba"
+  },
+  {
+    name: "Kabhi Mayoos mat hona",
+    artist: "Junaid-ur-Rehman",
+    src: "km"
+  },
+  {
+    name: "Haale Dil Kis Ko Sonaee",
+    artist: "Ghulam Mustafa Qadri",
+    src: "haledil"
+  },
+];
